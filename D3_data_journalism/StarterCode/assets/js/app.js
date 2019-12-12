f497f23bb291c3d188625b20c8b3a8704f91fb5d
unknown <fender190@hotmail.com>
2019-12-11 23:20:19 -0500

    svg dimensions and chart object defined

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -1,1 +1,13 @@
 // @TODO: YOUR CODE HERE!
+
+// SVG dimensions defined
+var svgWidth = 960;
+var svgHeight = 500;
+
+// Chart object defined
+var margin = {
+  top: 60,
+  right: 60,
+  bottom: 120,
+  left: 150
+};
