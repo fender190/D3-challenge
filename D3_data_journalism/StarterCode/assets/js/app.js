8e820f616740b209713fbd1238ece72aa4ef5d0c
unknown <fender190@hotmail.com>
2019-12-11 23:43:22 -0500

    Updated

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -11,3 +11,26 @@ var margin = {
   bottom: 120,
   left: 150
 };
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// svg
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// group charts
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  D3.csv("/assets/data/data.csv").then(function(myData) {
+
+    // number conversion
+    myData.forEach(function(xdata) {
+        xdata.poverty = +xdata.poverty;
+        xdata.healthcare = +xdata.healthcare;
+  },)});
+
